886ff6bc11f2b1bb084cd182590c3e7ed80b5391
Daniel Stegen <1272638+dstegen@users.noreply.github.com>
2021-10-20 21:26:59 +0200

    add/update user is working now

--- user/models/model-user.js
+++ user/models/model-user.js
@@ -11,11 +11,10 @@
 const path = require('path');
 const fs = require('fs');
 const bcrypt = require('bcryptjs');
-const locale = require('../../lib/locale');
-const config = require('../../main/models/model-config').getConfig();
 const { dateIsRecent } = require('../../lib/dateJuggler');
 const loadFile = require('../../utils/load-file');
 const saveFile = require('../../utils/save-file');
+const sani = require('../../utils/sanitizer');
 
 let users = [];
 
@@ -41,27 +40,15 @@ function getUserDetails (userId) {
   }
 }
 
-function getAllUsers (group) {
-  if (group !== undefined && group !== '') {
-    return users.filter( user => user.group === group );
-  } else {
-    return users;
-  }
+function getAllUsers () {
+  return users;
 }
 
-function usersOnline (group) {
+function usersOnline () {
   let onlineUsers = [];
   try {
     let sessionIds = JSON.parse(fs.readFileSync(path.join(__dirname, '../../sessionids.json')));
-    let userIds = sessionIds.map( user => { return Object.values(user)[0]; } )
-    if ( group !== undefined && group !== '') {
-      let allUsers = getAllUsers(group).filter( user => (user.role === 'student' && userIds.includes(user.userId)) );
-      allUsers.forEach(user => {
-        onlineUsers.push(user.fname+' '+user.lname);
-      });
-    } else {
-      onlineUsers = userIds.length;
-    }
+    onlineUsers = sessionIds.map( user => { return Object.values(user)[0]; } ).length;
   } catch (e) {
     console.log('- ERROR reading determing online students: '+e);
   }
@@ -93,24 +80,6 @@ function getUserFullName (id) {
   }
 }
 
-function getTitleNameById (id, n=false) {
-  if (id !== '' && typeof(id) === 'number') {
-    let user = users.filter( user => user.id === id)[0];
-    if (user.role === 'student') {
-      return user.fname + ' ' + user.lname + ', ' + user.group;
-    } else if (user.gender && user.gender === 'male' && n) {
-      return locale.headlines.title_mr_n[config.lang]+' ' + user.lname;
-    } else if (user.gender && user.gender === 'male') {
-      return locale.headlines.title_mr[config.lang]+' ' + user.lname;
-    } else if (user.gender && user.gender === 'female') {
-      return locale.headlines.title_ms[config.lang]+' ' + user.lname;
-    } else {
-      return locale.headlines.title_div[config.lang]+' ' + user.lname;
-    }
-  } else {
-    return {}
-  }
-}
 
 function updateUser (fields) {
   if (fields.id !== '' && fields.userId !== '') {
@@ -118,67 +87,40 @@ function updateUser (fields) {
     let tmpObj = users.filter( user => user.id === Number(fields.id))[0];
     Object.keys(fields).forEach( key => {
       if (key !== 'id' && key !== 'password' && fields[key] !== '') {
-        if (key === 'group' && fields.role === 'member' && typeof(fields.group) === 'string') {
-          tmpObj.group = fields.group.split(',');
-        } else if (key === 'courses' && fields.role === 'member' && typeof(fields.courses) === 'string') {
-          tmpObj.courses = fields.courses.split(',');
-        } else if (key === 'leader' && fields.role === 'member' && typeof(fields.leader) === 'string') {
-          tmpObj.leader = fields.leader.split(',');
-        } else {
-          tmpObj[key] = fields[key];
-        }
+        tmpObj[key] = sani(fields[key]);
       }
     });
   } else if (fields.userId !== '') {
     // add user
     let myPassword = '123';
-    if (fields.password != '' && typeof(fields.password) === 'string') {
+    if (fields.password !== '' && typeof(fields.password) === 'string') {
       myPassword = fields.password;
     }
     users.push({
-      userId: fields.userId,
+      userId: sani(fields.userId),
       id: getNewId(users),
       password: bcrypt.hashSync(myPassword),
-      role: fields.role,
-      group: fields.role === 'member' ? fields.group.split(',') : fields.group,
-      leader: fields.role === 'member' ? fields.leader.split(',') : fields.leader,
-      courses: fields.role === 'member' ? fields.courses.split(',') : '',
-      fname: fields.fname,
-      lname: fields.lname,
-      email: fields.email,
-      phone: fields.phone,
-      gender: fields.gender
+      role: 'member',
+      fname: sani(fields.fname),
+      lname: sani(fields.lname),
+      email: sani(fields.email),
+      phone: sani(fields.phone)
     });
   }
-  saveFile(path.join(__dirname, '../../data/school'), 'users.json', users);
+  saveFile(path.join(__dirname, '../../data'), 'users.json', users);
   console.log('+ User sucessfully added/updated: '+fields.userId);
+  initUsers();
 }
 
 function updatePassword (fields) {
   let curUser = users.filter( user => user.userId === fields.userId)[0];
   if (curUser !== undefined) {
     curUser.password = bcrypt.hashSync(fields.new_password);
-    saveFile(path.join(__dirname, '../../data/school'), 'users.json', users);
+    saveFile(path.join(__dirname, '../../data'), 'users.json', users);
     console.log('+ Password sucessfully updated for userId: '+fields.userId);
   }
 }
 
-function advanceUsers (fields) {
-  let allUsers = getAllUsers();
-  allUsers.filter( user => user.role === 'student' && user.group === fields.oldGroup).forEach( item => {
-    item.group = fields.newGroup;
-  });
-  allUsers.filter( user => user.role === 'member' && user.group.includes(fields.oldGroup)).forEach( item => {
-    item.group.push(fields.newGroup);
-    item.group = item.group.filter(item => item !== fields.oldGroup);
-    if (item.leader.includes(fields.oldGroup)) {
-      item.leader.push(fields.newGroup);
-      item.leader = item.leader.filter(item => item !== fields.oldGroup);
-    }
-  });
-  saveFile(path.join(__dirname, '../../data/school'), 'users.json', allUsers);
-  console.log('+ Advanced useres from group '+fields.oldGroup+' to new group '+fields.newGroup);
-}
 
 
 // Additional functions
@@ -192,4 +134,4 @@ function getNewId (users) {
 }
 
 
-module.exports = { initUsers, getPasswdObj, getUserDetails, getAllUsers, usersOnline, cleanLogins, getUserById, getTitleNameById, updateUser, updatePassword, advanceUsers, getUserFullName };
+module.exports = { initUsers, getPasswdObj, getUserDetails, getAllUsers, usersOnline, cleanLogins, getUserById, updateUser, updatePassword, getUserFullName };
--- user/views/setpassword-view.js
+++ user/views/setpassword-view.js
@@ -15,31 +15,37 @@ const config = require('../../main/models/model-config').getConfig();
 function setpasswordView (userId, message='') {
   return `
    <!-- Setpassword-form -->
-    <div class="container d-flex justify-content-center py-5">
-      <div class="border w-md-50">
-        <div class="card-body">
-          <h2 class="mb-3">${locale.headlines.set_new_password[config.lang]}</h2>
-          <form action="/updatepassword" method="post" enctype="application/x-www-form-urlencoded" name="setpassword-form">
-            <input type="text" readonly class="d-none" id="userId" name="userId" value="${userId}">
-            <div class="form-group">
-              <label for="login-password">${locale.placeholder.old_password[config.lang]}</label>
-              <input type="password" class="form-control" id="password" name="password" placeholder="${locale.placeholder.old_password[config.lang]}" required>
-            </div>
-            <div class="form-group">
-              <label for="login-password">${locale.placeholder.new_password[config.lang]}</label>
-              <input type="password" class="form-control" id="new_password" name="new_password" placeholder="${locale.placeholder.new_password[config.lang]}" required>
-            </div>
-            <div class="form-group">
-              <label for="login-password">${locale.placeholder.retype_password[config.lang]}</label>
-              <input type="password" class="form-control" id="retype_password" name="retype_password" onKeyUp="isPasswordMatch();" placeholder="${locale.placeholder.retype_password[config.lang]}" required>
-              <div id="divCheckPassword" class="small text-danger"></div>
-            </div>
-            <div class=" d-flex justify-content-end">
-              <button type="submit" class="btn btn-secondary">
-                ${locale.buttons.update[config.lang]}
-              </button>
-            </div>
-          </form>
+    <div class="container py-3">
+      <h2 class="d-flex justify-content-between py-2 px-3 my-3 border">
+        User settings
+        <span id="clock" class="d-none d-md-block">&nbsp;</span>
+      </h2>
+      <div class="d-flex justify-content-center">
+        <div class="border w-md-50">
+          <div class="card-body">
+            <h2 class="mb-3">${locale.headlines.set_new_password[config.lang]}</h2>
+            <form action="/updatepassword" method="post" enctype="application/x-www-form-urlencoded" name="setpassword-form">
+              <input type="text" readonly class="d-none" id="userId" name="userId" value="${userId}">
+              <div class="form-group">
+                <label for="login-password">${locale.placeholder.old_password[config.lang]}</label>
+                <input type="password" class="form-control mt-1" id="password" name="password" placeholder="${locale.placeholder.old_password[config.lang]}" required>
+              </div>
+              <div class="form-group mt-2">
+                <label for="login-password">${locale.placeholder.new_password[config.lang]}</label>
+                <input type="password" class="form-control mt-1" id="new_password" name="new_password" placeholder="${locale.placeholder.new_password[config.lang]}" required>
+              </div>
+              <div class="form-group mt-2">
+                <label for="login-password">${locale.placeholder.retype_password[config.lang]}</label>
+                <input type="password" class="form-control mt-1" id="retype_password" name="retype_password" onKeyUp="isPasswordMatch();" placeholder="${locale.placeholder.retype_password[config.lang]}" required>
+                <div id="divCheckPassword" class="small text-danger"></div>
+              </div>
+              <div class=" d-flex justify-content-end mt-3">
+                <button type="submit" class="btn btn-secondary">
+                  ${locale.buttons.update[config.lang]}
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
